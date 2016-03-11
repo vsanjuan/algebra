0dea6a0c95837bc3bd423caeedcbc02c1385b8cf
vsanjuan <salvador.sanjuan@gmail.com>
2016-03-11 01:30:26 +0100

    feat: calculat angle between vectors

--- vector.py
+++ vector.py
@@ -1,3 +1,5 @@
+import math
+
 class Vector(object):
     def __init__(self, coordinates):
         try:
@@ -92,36 +94,71 @@ class Vector(object):
             raise ValueError('The dot product must be between vectors with the \
                            same dimensions.')
 
+    def angle(self, j):
+        # Calculate the angle between two vectors as the arcsin of the
+        # division between the dot product and the product of the magnitudes of
+        # the vectors.
+        try:
+            if not type(j) == Vector:
+                raise ValueError
+            if not self.dimension == j.dimension:
+                raise ValueError
 
-a = Vector([8.218,-9.341])
-b = Vector([-1.129,2.111])
+            dot_product = self.dot(j)
+            magnitudes = self.magnitude() * j.magnitude()
 
-c = Vector([7.119,8.215])
-d = Vector([-8.223,0.878])
+            return math.acos( dot_product / magnitudes)
 
-e = Vector([1.671,-1.012,-0.318])
-
-f = Vector([-0.221,7.437])
-g = Vector([5.581,-2.136])
-
-h = Vector([8.813,-1.331,-6.247])
-j = Vector([1.996,3.108,-4.554])
+        except ValueError:
+            raise ValueError('To calculate the angle between two vectors must \
+                            have the same dimensions.')
 
 
-print a + b
-print c - d
-print e * 7.41
-print f.magnitude()
-print h.magnitude()
-print g.normal()
-print j.normal()
+# a = Vector([8.218,-9.341])
+# b = Vector([-1.129,2.111])
+#
+# c = Vector([7.119,8.215])
+# d = Vector([-8.223,0.878])
+#
+# e = Vector([1.671,-1.012,-0.318])dot_product/
+#
+# f = Vector([-0.221,7.437])
+# g = Vector([5.581,-2.136])
+#
+# h = Vector([8.813,-1.331,-6.247])
+# j = Vector([1.996,3.108,-4.554])
+#
+#
+# print a + b
+# print c - d
+# print e * 7.41
+# print f.magnitude()
+# print h.magnitude()
+# print g.normal()
+# print j.normal()
+#
+# print g.normal().magnitude()
+# print j.normal().magnitude()
+#
+# # dot product
+# k,j = Vector([7.887,4.138]),Vector([-8.802,6.776])
+# l,m = Vector([-5.955,-4.904,-1.874]),Vector([-4.496,-8.755,7.103])
+#
+# print k.dot(j)
+# print l.dot(m)
 
-print g.normal().magnitude()
-print j.normal().magnitude()
+# angle between vectors
+n,o = Vector([1,1]),Vector([2,2])
+q = Vector([-1,-1])
 
-# dot product
-k,j = Vector([7.887,4.138]),Vector([-8.802,6.776])
-l,m = Vector([-5.955,-4.904,-1.874]),Vector([-4.496,-8.755,7.103])
+print n.dimension, o.dimension
+print type(o) == Vector
 
-print k.dot(j)
-print l.dot(m)
+print math.degrees(n.angle(o))
+print math.degrees(n.angle(q))
+
+r, s = Vector([3.183,-7.627]),Vector([-2.668,5.319])
+t, u = Vector([7.35,0.221,5.188]),Vector([2.751,8.259,3.985])
+
+print r.angle(s)
+print math.degrees(t.angle(u))
