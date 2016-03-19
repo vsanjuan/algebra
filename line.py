c86d8148907ba2c8030e0c3d0586061a2702ecf4
vsanjuan <salvador.sanjuan@gmail.com>
2016-03-19 12:36:36 +0100

    error: Line init fails because helper function tries to iterate on the Normal vector. Commit before trying to solve the problem. Base point for any corrections.

---
+++ line.py
@@ -0,0 +1,132 @@
+from decimal import Decimal, getcontext
+
+from vector import Vector
+
+getcontext().prec = 30
+
+
+class Line(object):
+
+    NO_NONZERO_ELTS_FOUND_MSG = 'No nonzero elements found'
+
+    def __init__(self, normal_vector=None, constant_term=None):
+        # Normal vector is made up from the coeficients of the line equation and
+        # the constant from its constant. As such the normal vector is a vector
+        # object
+        self.dimension = 2
+
+        if not normal_vector:
+            all_zeros = ['0']*self.dimension
+            normal_vector = Vector(all_zeros)
+        self.normal_vector = normal_vector
+
+        if not constant_term:
+            constant_term = Decimal('0')
+        self.constant_term = Decimal(constant_term)
+
+        self.set_basepoint()
+
+
+    def set_basepoint(self):
+        try:
+            n = self.normal_vector
+            c = self.constant_term
+            basepoint_coords = ['0']*self.dimension
+
+
+            initial_index = Line.first_nonzero_index(n)
+            initial_coefficient = n[initial_index]
+
+            basepoint_coords[initial_index] = c/initial_coefficient
+            self.basepoint = Vector(basepoint_coords)
+
+        except Exception as e:
+            if str(e) == Line.NO_NONZERO_ELTS_FOUND_MSG:
+                self.basepoint = None
+            else:
+                raise e
+
+
+    def __str__(self):
+
+        num_decimal_places = 3
+
+        def write_coefficient(coefficient, is_initial_term=False):
+            coefficient = round(coefficient, num_decimal_places)
+            if coefficient % 1 == 0:
+                coefficient = int(coefficient)
+
+            output = ''
+
+            if coefficient < 0:
+                output += '-'
+            if coefficient > 0 and not is_initial_term:
+                output += '+'
+
+            if not is_initial_term:
+                output += ' '
+
+            if abs(coefficient) != 1:
+                output += '{}'.format(abs(coefficient))
+
+            return output
+
+        n = self.normal_vector
+
+        try:
+            initial_index = Line.first_nonzero_index(n)
+            terms = [write_coefficient(n[i], is_initial_term=(i==initial_index)) + 'x_{}'.format(i+1)
+                     for i in range(self.dimension) if round(n[i], num_decimal_places) != 0]
+            output = ' '.join(terms)
+
+        except Exception as e:
+            if str(e) == self.NO_NONZERO_ELTS_FOUND_MSG:
+                output = '0'
+            else:
+                raise e
+
+        constant = round(self.constant_term, num_decimal_places)
+        if constant % 1 == 0:
+            constant = int(constant)
+        output += ' = {}'.format(constant)
+
+        return output
+
+
+        def paralel(self,line2):
+            # This function takes as input another line a checks if is paralel
+            # to it by comparing its normal vectors.
+            return 0 == self.normal_vector.dot(line2.normal_vector)
+
+        def equal(self,line2):
+            if self.paralel(line2):
+                return False
+            line1_base_point = self.basepoint
+            line2_base_point = self.basepoint
+            x = line1_base_point - line2_base_point
+            return self.normal_vector.dot(x)
+
+    @staticmethod
+    def first_nonzero_index(iterable):
+        for k, item in enumerate(iterable):
+            if not MyDecimal(item).is_near_zero():
+                return k
+        raise Exception(Line.NO_NONZERO_ELTS_FOUND_MSG)
+
+
+
+
+class MyDecimal(Decimal):
+    def is_near_zero(self, eps=1e-10):
+        return abs(self) < eps
+
+
+# Check paralel method
+x = Line(Vector(2,2,),3)
+y = Line(Vector(2,2),6))
+z = Line(Vector(4,4,),3)
+
+print x.paralel(y)
+print x.paralel(z)
+print x.equal(y)
+print x.equal(z)
